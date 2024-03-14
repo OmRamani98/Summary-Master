d307582ed91dcd1d3483c59de8b705cd9a0d7153
OM RAMANI <omramani98@gmail.com>
2024-03-14 15:29:45 +0530

    optimized

--- src/Components/DataFetcherText.js
+++ src/Components/DataFetcherText.js
@@ -1,5 +1,5 @@
-import React, { useEffect ,useState} from "react";
-import Keywords from "./keyword";
+import React, { useEffect } from "react";
+
 
 let keyset;
 const DataFetcherText = ({ text, sliderValue, onFetchComplete }) => {
